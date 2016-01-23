c96eb510c5e5ffdd0ed10e63cbe7e5971c7a4750
Charles Tao <ctao@calpoly.edu>
2016-01-23 14:42:07 -0800

    initial setup w/ heroku

---
+++ src/main/java/Server.java
@@ -0,0 +1,29 @@
+import java.sql.*;
+import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.Map;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import static spark.Spark.*;
+import spark.template.freemarker.FreeMarkerEngine;
+import spark.ModelAndView;
+import static spark.Spark.get;
+
+import com.heroku.sdk.jdbc.DatabaseUrl;
+
+import static javax.measure.unit.SI.KILOGRAM;
+import javax.measure.quantity.Mass;
+import org.jscience.physics.model.RelativisticModel;
+import org.jscience.physics.amount.Amount;
+
+public class Server {
+
+   public static void main(String[] args) {
+
+
+   }
+
+}
+
