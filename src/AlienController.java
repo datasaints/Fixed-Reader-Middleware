028803a6639046d076041a62a8aaa851fe63d9a2
Bobby Jones <rjones11@calpoly.edu>
2016-01-23 00:35:41 -0800

    FixedReaderApp: Initial commit
    Added base eclipse (Mars) project containing the AlienRFID library provided
    by AlienTechnologies for the ALR-9900+ passive RFID reader.
    
    Signed off by: Robert Jones <soliis.jones@gmail.com>

---
+++ src/AlienController.java
@@ -0,0 +1,64 @@
+
+
+import java.net.UnknownHostException;
+
+import com.alien.enterpriseRFID.reader.AlienClass1Reader;
+import com.alien.enterpriseRFID.reader.AlienReaderException;
+
+public class AlienController {
+   private AlienClass1Reader reader;
+   
+   private String ipAddress;
+   private int portNumber;
+   private String username;
+   private String password;
+
+   private final int RF_LEVEL = 167;
+   private String tagMask = "E200 XXXX XXXX XXXX XXXX XXXX";
+
+   /**
+    * Constructor
+    * @param ipAddress
+    * @param portNumber
+    * @param username
+    * @param password
+    * @throws UnknownHostException
+    * @throws AlienReaderException
+    */
+   public AlienController(String ipAddress, int portNumber, String username, String password) {
+      this.ipAddress = ipAddress;
+      this.portNumber = portNumber;
+      this.username = username;
+      this.password = password;
+   }
+
+   /**
+    * Initialize reader settings and verify reader connection
+    * @throws AlienReaderException 
+    * @throws UnknownHostException 
+    */
+   public void initializeReader() throws UnknownHostException, AlienReaderException {
+      // Establish user parameters
+      reader = new AlienClass1Reader(ipAddress, portNumber);
+      reader.setUsername(username);
+      reader.setPassword(password);
+
+      // Establish connection and clear any initial tags read
+      reader.open();
+      reader.clearTagList();
+
+      // Establish behavioral parameters
+      reader.setAutoMode(AlienClass1Reader.OFF);
+      reader.setRFLevel(RF_LEVEL);
+      reader.setTagMask(tagMask);
+      reader.setTagListFormat(AlienClass1Reader.TEXT_FORMAT);
+      reader.setTagStreamFormat(AlienClass1Reader.TEXT_FORMAT);
+      reader.setTagListMillis(AlienClass1Reader.ON);
+   }
+
+   // Return reader
+   public AlienClass1Reader getReader() {
+      return this.reader;
+   }
+
+}
