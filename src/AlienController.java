7a5e9d272254157acd7e6bf88698a18435078f36
Charles Tao <ctao@calpoly.edu>
2016-01-23 16:18:20 -0800

    formatting tabs

--- src/AlienController.java
+++ src/AlienController.java
@@ -7,7 +7,7 @@ import com.alien.enterpriseRFID.reader.AlienReaderException;
 
 public class AlienController {
    private AlienClass1Reader reader;
-   
+
    private String ipAddress;
    private int portNumber;
    private String username;
@@ -17,14 +17,14 @@ public class AlienController {
    private String tagMask = "E200 XXXX XXXX XXXX XXXX XXXX";
 
    /**
-    * Constructor
-    * @param ipAddress
-    * @param portNumber
-    * @param username
-    * @param password
-    * @throws UnknownHostException
-    * @throws AlienReaderException
-    */
+   * Constructor
+   * @param ipAddress
+   * @param portNumber
+   * @param username
+   * @param password
+   * @throws UnknownHostException
+   * @throws AlienReaderException
+   */
    public AlienController(String ipAddress, int portNumber, String username, String password) {
       this.ipAddress = ipAddress;
       this.portNumber = portNumber;
@@ -33,21 +33,21 @@ public class AlienController {
    }
 
    /**
-    * Initialize reader settings and verify reader connection
-    * @throws AlienReaderException 
-    * @throws UnknownHostException 
-    */
+   * Initialize reader settings and verify reader connection
+   * @throws AlienReaderException
+   * @throws UnknownHostException
+   */
    public void initializeReader() throws UnknownHostException, AlienReaderException {
-      // Establish user parameters
+   // Establish user parameters
       reader = new AlienClass1Reader(ipAddress, portNumber);
       reader.setUsername(username);
       reader.setPassword(password);
 
-      // Establish connection and clear any initial tags read
+   // Establish connection and clear any initial tags read
       reader.open();
       reader.clearTagList();
 
-      // Establish behavioral parameters
+   // Establish behavioral parameters
       reader.setAutoMode(AlienClass1Reader.OFF);
       reader.setRFLevel(RF_LEVEL);
       reader.setTagMask(tagMask);
--- src/NetworkDiscover.java
+++ src/NetworkDiscover.java
@@ -3,31 +3,31 @@ import com.alien.enterpriseRFID.discovery.DiscoveryListener;
 import com.alien.enterpriseRFID.discovery.NetworkDiscoveryListenerService;
 
 class NetworkDiscover implements DiscoveryListener {
-	
-	public NetworkDiscover() throws Exception {
-		NetworkDiscoveryListenerService service = new NetworkDiscoveryListenerService();
-		service.setDiscoveryListener(this);
-		service.startService();
-		
-		while (service.isRunning()) {
-			Thread.sleep(100);
-		}
-	}
-	
-	public void readerAdded(DiscoveryItem ds) {
-		System.out.println("Reader Added: " + ds.toString());
-		
-	}
 
-	@Override
-	public void readerRemoved(DiscoveryItem ds) {
-		System.out.println("Reader Removed: " + ds.toString());
-	}
+   public NetworkDiscover() throws Exception {
+      NetworkDiscoveryListenerService service = new NetworkDiscoveryListenerService();
+      service.setDiscoveryListener(this);
+      service.startService();
 
-	@Override
-	public void readerRenewed(DiscoveryItem ds) {
-		System.out.println("Reader Renewed: " + ds.toString());
-		
-	}
-	
+      while (service.isRunning()) {
+         Thread.sleep(100);
+      }
+   }
+
+   public void readerAdded(DiscoveryItem ds) {
+      System.out.println("Reader Added: " + ds.toString());
+
+   }
+
+   @Override
+   public void readerRemoved(DiscoveryItem ds) {
+      System.out.println("Reader Removed: " + ds.toString());
+   }
+
+   @Override
+   public void readerRenewed(DiscoveryItem ds) {
+      System.out.println("Reader Renewed: " + ds.toString());
+
+   }
+
 }
